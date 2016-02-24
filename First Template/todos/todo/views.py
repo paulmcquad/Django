20dc31b129cdfb60dc1e46c405fb16242363eba5
Paul McQuade <paulmcquad@gmail.com>
2016-02-24 21:41:52 +0000

    My First Django template
    Todo work list
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ First Template/todos/todo/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.template import Context, loader
+from django.http import HttpResponse
+
+def hello_world(request):
+    todos = [ {'title': 'Mow the lawn',
+               'importance': 'Minor'},
+              {'title': 'Backup your PC',
+               'importance': 'High'},
+              {'title': 'Buy some Milk',
+               'importance': 'Medium'}, ]
+    t = loader.get_template('index.tmpl')
+    c = Context({
+        'todos': todos,
+    })
+    return HttpResponse(t.render(c))
+
